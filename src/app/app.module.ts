9db41e8988c17eb7fe23a3560f06324cedc9ea8a
Jo_wanja <jojodiva2@gmail.com>
2022-04-11 11:48:34 +0300

    adding quote details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetsComponent } from './quote-dets/quote-dets.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    QuoteDetsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quote-dets/quote-dets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-dets',
+  templateUrl: './quote-dets.component.html',
+  styleUrls: ['./quote-dets.component.css']
+})
+export class QuoteDetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
