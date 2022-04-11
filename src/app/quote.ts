f04337da7cfcd9ca79094d5cfe71b54763ed8ed1
Jo_wanja <jojodiva2@gmail.com>
2022-04-11 11:46:02 +0300

    adding quotes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,8 @@
 export class Quote {
+  quote!:string;
+  author!:string;
+  submitter!:string;
+  upvote!:number;
+  downvote!:number;
+
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  dispQuotes:Quote[]=[
+    {quote:'happy are the meek',author:'Jesus',submitter:'wanja',upvote:0,downvote:0},
+    {quote:'happy are the meek',author:'Jesus',submitter:'wanja',upvote:0,downvote:0},
+    {quote:'happy are the meek',author:'Jesus',submitter:'wanja',upvote:0,downvote:0},
+    {quote:'happy are the meek',author:'Jesus',submitter:'wanja',upvote:0,downvote:0},
+  ]
 
   constructor() { }
 
