85ffdbd7958884e5eb25b63d2809f0aa17aa3cd2
Jo_wanja <jojodiva2@gmail.com>
2022-04-11 17:31:23 +0300

    importing forms module in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,11 +14,13 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
     AppComponent,
     QuoteComponent,
     QuoteDetsComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
    
   ],
   providers: [],
